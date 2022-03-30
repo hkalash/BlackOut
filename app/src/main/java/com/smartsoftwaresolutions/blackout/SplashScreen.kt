cf59dce3ddffc2aa8e25add07a6f95c231bbb45a
hkalash <hkhkalash@gmail.com>
2022-03-30 13:58:33 +0300

    add a Splash Screen

---
+++ app/src/main/java/com/smartsoftwaresolutions/blackout/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.smartsoftwaresolutions.blackout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash_screen)
+
+
+
+        Handler().postDelayed(
+            {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            },3000)
+    }
+
+
+}
